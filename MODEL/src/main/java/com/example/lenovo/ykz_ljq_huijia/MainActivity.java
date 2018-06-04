21ec02ec09e666ed4bb53ebaa6b33f292119deaf
1275021828@QQ.COM <12750218282@QQ.com>
2018-06-04 17:05:00 +0800

    这是实验demo

---
+++ MODEL/src/main/java/com/example/lenovo/ykz_ljq_huijia/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.lenovo.ykz_ljq_huijia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
